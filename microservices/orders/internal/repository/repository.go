2f970fe50f470ea0ce16a6cfdfc5746ca99296ef
emptyhopes <literallymyfault@gmail.com>
2023-11-07 20:47:44 +0500

    update orders, orders-publisher, orders-subscriber

--- microservices/orders/internal/repository/repository.go
+++ microservices/orders/internal/repository/repository.go
@@ -4,13 +4,6 @@ import (
 	dto "github.com/emptyhopes/orders/internal/dto/orders"
 )
 
-//var Cache = storage.NewCache()
-//var Database = storage.NewDatabase()
-
-//func init() {
-//	Database.Initialize()
-//}
-
 type OrderRepositoryInterface interface {
 	GetOrderCache(string) (*dto.OrderDto, bool)
 	SetOrderCache(string, *dto.OrderDto)
